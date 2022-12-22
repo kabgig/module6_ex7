9d6c64517c4d926e03bf4ca7120bb2047c6f58ff
Ansar Ismagilov <ansar89kab@bk.ru>
2022-12-22 19:15:40 +0100

    Commit 8 - brach_1 change

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("branch_1");
+        System.out.println("branch_1111");
+        System.out.println("branch_1111");
+        System.out.println("branch_1111");
     }
 }
